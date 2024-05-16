127bfb12114e43446404b359c3d5515dbdcca4bf
ATYK <thu454909@gmail.com>
2024-05-16 14:07:25 +0630

    blog

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // prefix: 'tw-', // with prefix use "tw-bg-red-500"
+  content: ["./src/**/*.{html,js}"],
+  // content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
